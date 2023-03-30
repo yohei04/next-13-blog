33de32f67afe74274eb3802b4aefd964c4edd65c
Yohei Higashi <yoheihigashi.pg@gmail.com>
2023-03-30 16:58:34 +0900

    feat: add articles Route Handler and page

---
+++ src/app/api/articles/route.ts
@@ -0,0 +1,16 @@
+import { NextResponse } from 'next/server';
+
+import prisma from '@/lib/prisma';
+import { Article } from '@prisma/client';
+
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+export async function GET(_req: NextApiRequest, res: NextApiResponse<Article[]>) {
+  const articles = await prisma.article.findMany({
+    where: {
+      published: false,
+    },
+  });
+
+  return NextResponse.json(articles);
+}
---
+++ src/app/articles/page.tsx
@@ -0,0 +1,40 @@
+async function getData() {
+  const res = await fetch('http://localhost:3000/api/articles');
+  // The return value is *not* serialized
+  // You can return Date, Map, Set, etc.
+
+  // Recommendation: handle errors
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error('Failed to fetch data');
+  }
+
+  return res.json();
+}
+
+export default async function Home() {
+  // const articles = await prisma.article.findMany({
+  //   where: {
+  //     published: false,
+  //   },
+  // });
+
+  const articles = await getData();
+
+  console.log({ articles });
+
+  return (
+    <main>
+      <ul>
+        {articles.map((article: any) => (
+          <li key={article.id}>
+            <div>{article.id}</div>
+            <div>{article.title}</div>
+            <div>{article.body}</div>
+            <div>{article.updatedAt.toString()}</div>
+          </li>
+        ))}
+      </ul>
+    </main>
+  );
+}
