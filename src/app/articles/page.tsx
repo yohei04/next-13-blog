de2427a911c2e3e1fa9d2931f648246e5f35ccca
Yohei Higashi <yoheihigashi.pg@gmail.com>
2023-03-31 14:56:17 +0900

    fix: add baseUrl log

--- src/app/articles/page.tsx
+++ src/app/articles/page.tsx
@@ -8,6 +8,8 @@ async function getData(): Promise<Article[]> {
   // The return value is *not* serialized
   // You can return Date, Map, Set, etc.
 
+  console.log({ baseUrl });
+
   // Recommendation: handle errors
   if (!res.ok) {
     // This will activate the closest `error.js` Error Boundary
