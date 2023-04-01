11f9c446e5c01aad0be22a48325851c4bbf3c772
Yohei Higashi <yoheihigashi.pg@gmail.com>
2023-04-01 15:48:08 +0900

    feat: add likes and profile page

---
+++ src/app/articles/(articles)/layout.tsx
@@ -0,0 +1,33 @@
+import Link from 'next/link';
+
+import styles from './layout.module.css';
+
+type Props = {
+  children: React.ReactNode;
+};
+
+const HEADER_MENUS = [
+  { name: 'すべて', path: '/articles' },
+  // { name: 'Tech', path: '/tags/tech/articles' },
+  // { name: 'Idea', path: '/tags/tech/articles' },
+] as const;
+
+export default function Layout({ children }: Props) {
+  return (
+    <>
+      <header className={styles.header}>
+        <h2>ヘッダー:</h2>
+        <nav>
+          <ul className={styles.header__list}>
+            {HEADER_MENUS.map((headerMenu) => (
+              <li key={headerMenu.name}>
+                <Link href={headerMenu.path}>{headerMenu.name}</Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </header>
+      <div className={styles.content}>{children}</div>
+    </>
+  );
+}
---
+++ src/app/articles/profile/page.tsx
@@ -0,0 +1,9 @@
+export default async function Page() {
+  return (
+    <div>
+      <section>
+        <h1>プロフィール</h1>
+      </section>
+    </div>
+  );
+}
