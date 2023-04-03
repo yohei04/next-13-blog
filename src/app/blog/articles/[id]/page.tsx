513a91fcedd1da12756eb3dc760c762e38ba412b
Yohei Higashi <yoheihigashi.pg@gmail.com>
2023-04-04 03:24:55 +0900

    feat: add comment in an article page

--- src/app/blog/articles/[id]/page.tsx
+++ src/app/blog/articles/[id]/page.tsx
@@ -1,7 +1,7 @@
 import { baseUrl } from '@/lib/baseUrl';
-import { Article } from '@prisma/client';
+import { Article, Comment } from '@prisma/client';
 
-async function getData(id: string): Promise<Article> {
+async function getArticle(id: string): Promise<Article> {
   const res = await fetch(`${baseUrl}/api/blog/articles/${id}`);
 
   if (!res.ok) {
@@ -11,17 +11,44 @@ async function getData(id: string): Promise<Article> {
   return res.json();
 }
 
+async function getComments(id: string): Promise<Comment[]> {
+  const res = await fetch(`${baseUrl}/api/blog/articles/${id}/comments`);
+
+  if (!res.ok) {
+    throw new Error('Failed to fetch data');
+  }
+
+  return res.json();
+}
+
 export default async function Page({ params }: { params: { id: string } }) {
-  const article = await getData(params.id);
+  const articleData = getArticle(params.id);
+  const commentsData = getComments(params.id);
+
+  const [article, comments] = await Promise.all([articleData, commentsData]);
 
   return (
     <section>
       <h1>記事詳細</h1>
       <article key={article.id}>
-        <div>{article.id}</div>
-        <div>{article.title}</div>
-        <div>{article.body}</div>
-        <div>{article.updatedAt.toString()}</div>
+        <div>
+          <div>{article.id}</div>
+          <div>{article.title}</div>
+          <div>{article.body}</div>
+          <div>{article.updatedAt.toString()}</div>
+        </div>
+        <section>
+          <h2>コメント一覧</h2>
+          <ul>
+            {comments.map((comment) => (
+              <li key={comment.id}>
+                <div>{comment.id}</div>
+                <div>{comment.content}</div>
+                <div>{comment.updatedAt.toString()}</div>
+              </li>
+            ))}
+          </ul>
+        </section>
       </article>
     </section>
   );
