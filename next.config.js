34f82459373ca5255ec15062065b270e5bd31734
Yohei Higashi <yoheihigashi.pg@gmail.com>
2023-04-08 01:18:02 +0900

    chore: delete output: export

--- next.config.js
+++ next.config.js
@@ -1,10 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   experimental: {
     appDir: true,
     typedRoutes: true,
   },
 };
 
-module.exports = nextConfig
+module.exports = nextConfig;
